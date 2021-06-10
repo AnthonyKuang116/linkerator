b4447924a505d581fc40e84c903b15de0e5bcde4
addis g <addis.a.gn@gmail.com>
2021-06-09 21:05:23 -0400

    First Commit

--- db/index.js
+++ db/index.js
@@ -1,23 +1,22 @@
 // Connect to DB
-const { Client } = require('pg');
-const DB_NAME = 'change-this-name'
+const { Client } = require("pg");
+const DB_NAME = "change-this-name";
 const DB_URL = process.env.DATABASE_URL || `postgres://${DB_NAME}`;
 const client = new Client(DB_URL);
 
 // database methods
-async function createLink(
-  link,
-  clickCount,
-  comment,
-  dateShared,
-  linkId
-) {
+async function createLink(link, clickCount, comment, dateShared, linkId) {
   try {
-    const {rows: [links]} = await client.query(`
+    const {
+      rows: [links],
+    } = await client.query(
+      `
       INSERT INTO links (link, "clickCount", comment, "dateShared", "linkId)
       VALUES ($1, $2, $3, $4, $5)
       RETURNING *;
-    `, [link, clickCount, comment, dateShared, linkId]);
+    `,
+      [link, clickCount, comment, dateShared, linkId]
+    );
 
     return links;
   } catch (error) {
@@ -27,13 +26,28 @@ async function createLink(
 
 async function createTag(name) {
   try {
-    const {rows: [tag]} = await client.query(`
+    const {
+      rows: [tag],
+    } = await client.query(
+      `
       INSERT INTO tags (name)
       VALUES ($1)
       RETURNING *;
-    `, [name]);
+    `,
+      [name]
+    );
 
     return tag;
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function getAllTags() {
+  try {
+    const { rows: tags } = await client.query(`
+    SELECT *
+    FROM tags`);
   } catch (error) {
     throw error;
   }
@@ -43,4 +57,4 @@ async function createTag(name) {
 module.exports = {
   client,
   // db methods
-}
+};
